641c09c763e77ee71fb436719eef05b094a001f0
Zak Thompson <zak@fireinsidestudios.com>
2025-04-25 14:45:06 -0400

    LazyVim updates and asdf config

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install dependencies
-brew install zsh neovim ag yarn tree-sitter luajit ripgrep rustup fzf typewritten jordanbaird-ice
+brew install zsh neovim ag yarn tree-sitter luajit ripgrep rustup fzf typewritten jordanbaird-ice starship coreutils curl git asdf gpg gawk ffmpeg yt-dlp
 yarn global add stylelint eslint eslint_d prettier @fsouza/prettierd
 rustup-init -y
 
-# Install font
+# Install casks
 brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font kitty slack discord moonlight craft notion audacity dropbox localsend transmission
 
 # Set zsh as default shell
 chsh -s /usr/local/bin/zsh
@@ -19,7 +19,7 @@ rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.config/nvim ~/.config/kitty 2>/dev/null
 mkdir -p ~/.config ~/.config/nvim ~/.config/kitty
 
 # Install Oh My Zsh and themes/plugins
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
@@ -28,8 +28,9 @@ touch ~/.hushlogin
 
 # Symlink configs
 ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/nvim ~/.config/nvim
-ln -s $PWD/kitty ~/.config/kitty
+ln -s $PWD/asdfrc ~/.asdfrc
+ln -s $PWD/nvim ~/.config/
+ln -s $PWD/kitty ~/.config/
 
 # Use nvim for further configuration
 alias vim="nvim"
--- zshrc
+++ zshrc
@@ -70,7 +70,7 @@ ENABLE_CORRECTION="true"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git z fzf 1password git-flow-avh yarn brew zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(git z fzf 1password git-flow-avh yarn brew zsh-autosuggestions zsh-syntax-highlighting asdf)
 
 source $ZSH/oh-my-zsh.sh
 
